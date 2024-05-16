542b0aac09844fd65dbe6f819848da4e1701cc56
BrahimNAJI2002 <brahim.naji1-etu@etu.univh2c.ma>
2024-05-16 18:49:39 +0100

    backend + frontend

---
+++ backend/src/main/java/ma/enset/backend/dtos/AccountOperationDTO.java
@@ -0,0 +1,16 @@
+package ma.enset.backend.dtos;
+
+import lombok.Data;
+import ma.enset.backend.enums.OperationType;
+
+import java.util.Date;
+
+@Data
+public class AccountOperationDTO {
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    private String description;
+}
+
